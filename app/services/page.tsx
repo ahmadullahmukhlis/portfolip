b13210b55581aa42b997f880335a2208606da72b
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-07-30 20:20:23 -0700

    the problem of the page created is fixed

---
+++ app/services/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReadySection from "../components/ReadySection";
+
+const Services: React.FC = () => {
+  return (
+    <main>
+      <h1>sdffdf</h1>
+      <ReadySection />
+    </main>
+  );
+};
+
+export default Services;
