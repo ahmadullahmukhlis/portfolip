a734bb70560e4c0c4cb740bd06f2a31b6a11133b
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-07-31 02:48:12 -0700

    the problemf o the is fixed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,14 +9,16 @@ const Footer: React.FC = () => {
             <div className="col-lg-6">
               <div className="copyright-text">
                 <p>
-                  Copyright &copy; 2023, <a href="/">Bentos</a> All Rights
+                  Copyright &copy; 2024, <a href="/">Mukhlis</a> All Rights
                   Reserved.
                 </p>
               </div>
             </div>
             <div className="col-lg-6">
               <div className="copyright-text extra-copyright">
-                <p>Crafted with ❤️ by Tanvir Hossain</p>
+                <p>
+                  Designed with passion and precision by Ahmad Ullah Mukhlis❤️
+                </p>
               </div>
             </div>
           </div>
